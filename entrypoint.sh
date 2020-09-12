a86649443e1a62e41ec52417e7ee2f3d6e5cac42
Aakash Goel <aakash.lpin@gmail.com>
2020-09-12 17:02:55 +0530

    playing around

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,12 @@ route_id=$4
 worker=$5
 route_pattern=$6
 
-curl -X PUT "https://api.cloudflare.com/client/v4/zones/"$zone_id"/workers/routes/"$route_id"" \
--H "X-Auth-Email: "$email"" \
--H "X-Auth-Key: "$apikey"" \
+echo "Hello ${email}"
+echo "Updating route pattern to ${route_pattern}"
+curl -X PUT "https://api.cloudflare.com/client/v4/zones/"${zone_id}"/workers/routes/"${route_id}"" \
+-H "X-Auth-Email: "${email}"" \
+-H "X-Auth-Key: "${apikey}"" \
 -H "Content-Type: application/json" \
---data '{"pattern":"'"$route_pattern"'","script":"'"$worker"'"}'
+--data '{"pattern":"'"${route_pattern}"'","script":"'"${worker}"'"}'
 
 exit 0
